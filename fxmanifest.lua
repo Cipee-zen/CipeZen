0e8691bfdeb377d220143c0fe79334891d50dc4b
Editz7 <Adam.east.tx@gmail.com>
2022-06-25 21:21:56 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+game "gta5"
+fx_version "cerulean"
+
+client_scripts  {
+    "client/controls.lua",
+    "client/client.lua",
+}
+
+server_scripts  {
+    "@mysql-async/lib/MySQL.lua",
+    "server/server.lua",
+    "server/commands.lua",
+}
+
+
+ui_page "html/index.html"
+
+files {
+    "html/index.html",
+    "html/styles.css",
+    "html/main.js",
+}
+
+dependencies {
+    "c_menu_z",
+}
