03128e58a9225e22f4e1c8462c370e6add590f58
Cipee <mike15intanno@gmail.com>
2022-07-14 03:57:03 +0200

    add color change in HelpNotify

--- html/main.js
+++ html/main.js
@@ -71,6 +71,44 @@ function Notify(title, description, _time) {
 function HelpNotify(title, description, _time) {
     let time = _time || 5000;
     let randomInt = Math.floor(Math.random() * 1000000);
+    let color = title.match(/~[^~]*~/g);
+    let color2 = description.match(/~[^~]*~/g);
+    let openSpan = false
+    let openSpan2 = false
+    if (color) {
+        for (let i = 0; i < color.length; i++) {
+            let d = color[i].replace(/~/g, "");
+            if (d == "reset") {
+                d = "white";
+            }
+            if (i == 0) {
+                title = title.replace(color[i], `<span style="color: ${d}">`);
+            } else {
+                title = title.replace(color[i], `</span><span style="color: ${d}">`);
+                openSpan = true
+            }
+        }
+        if (openSpan) {
+            title = title + "</span>"
+        }
+    }
+    if (color2) {
+        for (let i = 0; i < color2.length; i++) {
+            let d = color2[i].replace(/~/g, "");
+            if (d == "reset") {
+                d = "#5a6253";
+            }
+            if (i == 0) {
+                description = description.replace(color2[i], `<span style="color: ${d}">`);
+            } else {
+                description = description.replace(color2[i], `</span><span style="color: ${d}">`);
+                openSpan2 = true
+            }
+        }
+        if (openSpan2) {
+            description = description + "</span>"
+        }
+    }
     helpNotifyContainer.innerHTML = `
         <div data-aos-anchor="#parent" class="hnotify" notify="${randomInt}">
             <div class="notify-img">
