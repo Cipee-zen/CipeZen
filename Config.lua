b634f9c1ff56f0c1f90d0d7748034a97e7e82ce1
Cipee <mike15intanno@gmail.com>
2022-07-21 06:37:10 +0200

    fix config spawn position

--- Config.lua
+++ Config.lua
@@ -1,3 +1,5 @@
 Config = {}
 
 Config.identifer = "license" --steam , license , xbl , ip , discord , live
+
+Config.SpawnPosition = {x =448.7125,y = -1021.1075,z = 28.4266,h = 279.6156}
--- server/server.lua
+++ server/server.lua
@@ -1,8 +1,6 @@
 local path = GetResourcePath(GetCurrentResourceName())
 local CZPlayersLoad = {}
 CZ = {}
-Config = {}
-Config.SpawnPosition = {x =448.7125,y = -1021.1075,z = 28.4266,h = 279.6156}
 
 CZServerCallBack = {}
 CipeZenPlayers = {}
